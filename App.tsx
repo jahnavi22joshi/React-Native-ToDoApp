667fa03284c897d7b8d6816d6c1f0790eaa14722
jahnavi22joshi <jahnavij1705@gmail.com>
2025-07-08 10:06:45 +0530

    add to the git

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {Text, View} from 'react-native';
 
-const App = () => {
+const YourApp = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+      }}>
+      <Text style={{
+        color:'white'
+        
+      }}>TO DO App 🎉</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default YourApp;
